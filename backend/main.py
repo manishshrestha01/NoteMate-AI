854a3489d727afb44312cda3318ecb51fa0d5fa8
manishshrestha01 <shresthamanish8080@gmail.com>
2025-07-17 16:14:22 +0545

    rr

--- backend/main.py
+++ backend/main.py
@@ -159,7 +159,6 @@ origins = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://project-git-main-manishshrestha01s-projects.vercel.app/"
 ]
 
 app.add_middleware(
